ed545c85b6f3a3281651da4126212f57544d0f55
Phillip Langley <philliplangley@champmbplangley.lan>
2022-06-08 14:13:34 -0500

    use jira lib instead of api

--- jira-notification.py
+++ jira-notification.py
@@ -1,81 +1,64 @@
 #!/usr/bin/python3
-#	usage: jira-notification.py
-# export JIRA_TOKEN, JIRA_HOST, JIRA_PROJECT and JIRA_REPO as env variable
+#	usage: python jira-notification.py
+# export JIRA_TOKEN, JIRA_HOST, JIRA_PROJECT, JIRA_USER and GIT_REPO as env variable
 #########################################################################################################
-
-
-import requests
-import json
-import sys
+# coding=utf-8
+from atlassian import Jira
 import os
 
-repo = os.environ["JIRA_REPO"]
-jiraproject = os.environ["JIRA_PROJECT"]
-jiratoken = os.environ["JIRA_TOKEN"]
-jirahost = os.environ["JIRA_HOST"]
+def jira_auth(host, user, token):
+    # jira auth requirements
+    jira = Jira(
+        url="https://" + host + "",
+        username="" + user + "",
+        password="" + token + "",
+        cloud=True)
+    return jira
 
-def check_existing_issue():
-    url = "https://" + jirahost + "/rest/api/3/search?jql=project%20%3D%20" + jiraproject + "%20AND%20labels%20%3D%20unit_test%20AND%20labels%20%3D%20" + repo + "%20and%20(statusCategory%20%3D%20\"To%20Do\"%20OR%20statusCategory%20%3D%20\"In%20Progress\")"
+def check_existing_issue(jira, project, repo):
+    # default jira JQL query string
+    JQL = "project = " + project + " AND labels = " + repo + " AND labels = unit_test AND (statusCategory = 'To Do' OR statusCategory = 'In Progress')"
 
-    payload = ""
-    headers = {
-        'Authorization': 'Basic ' + jiratoken + "'",
-        'Content-Type': 'application/json',
-    }
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-    get_total_issue_count = response.json().get("total")
+    data = jira.jql(JQL)
+    # checking json total value
+    get_total_issue_count = data["total"]
 
     return get_total_issue_count
 
-def create_jira_issue():
-    url = "https://" + jirahost + "/rest/api/3/issue"
-
-    payload = json.dumps({
-        "update": {},
-        "fields": {
-            "summary":  "" + repo + " unit test failure",
-            "issuetype": {
-                "name": "Problem"
-            },
+def create_jira_issue(jira, project, repo):
+    jira.issue_create(
+        fields={
             "project": {
-                "key": jiraproject
+                "key": project
             },
             "labels": [
                 "" + repo + "",
                 "unit_test"
             ],
-            "description": {
-                "type": "doc",
-                "version": 1,
-                "content": [
-                    {
-                        "type": "paragraph",
-                        "content": [
-                            {
-                                "text": "unit test module failure",
-                                "type": "text"
-                            }
-                        ]
-                    }
-                ]
-            }
+            "issuetype": {
+                "name": "Problem"
+            },
+            "summary": "" + repo + " failure",
+            "description": "unit test module failure",
         }
-    })
-    headers = {
-        'Authorization': 'Basic ' + jiratoken + "'",
-        'Content-Type': 'application/json',
-    }
+    )
 
-    response = requests.request("POST", url, headers=headers, data=payload)
-    print(response)
 
 def main():
+    repo = os.environ["GIT_REPO"]
+    jiraproject = os.environ["JIRA_PROJECT"]
+    jiratoken = os.environ["JIRA_TOKEN"]
+    jirahost = os.environ["JIRA_HOST"]
+    jirauser = os.environ["JIRA_USER"]
+
+    # auth to jira endpoint using host, user and token
+    get_jira_auth = jira_auth(jirahost, jirauser, jiratoken)
     # checking if issue exist, creating issue if total count equals zero
-    get_issue_count = check_existing_issue()
+    get_issue_count = check_existing_issue(get_jira_auth, jiraproject, repo)
     if get_issue_count == 0:
-        create_jira_issue()
+        create_jira_issue(get_jira_auth, jiraproject, repo)
     else:
         print('not creating issue, already exist........')
+
 
 main()
